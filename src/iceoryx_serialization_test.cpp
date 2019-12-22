5c32c51ba4f3dd0d02c63ed6507a39dbad878dfd
Knese Karsten <karsten@openrobotics.org>
2019-12-22 18:28:25 +0100

    something is wrong with nested messages

--- src/iceoryx_serialization_test.cpp
+++ src/iceoryx_serialization_test.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <chrono>
 
+#include "rclcpp/rclcpp.hpp"
+
 #include "rosidl_typesupport_introspection_cpp/identifier.hpp"
 #include "rosidl_typesupport_introspection_cpp/message_introspection.hpp"
 
-#include "rclcpp/rclcpp.hpp"
+#include "rmw_iceoryx_cpp/iceoryx_serialize.hpp"
+#include "rmw_iceoryx_cpp/iceoryx_type_info_introspection.hpp"
 
 #include "test_msgs/message_fixtures.hpp"
-
-#include "rmw_iceoryx_cpp/iceoryx_serialize.hpp"
 
 template<class MSG_T>
 void serialize(void * msg)
@@ -50,15 +51,59 @@ void measure_serialize(void * msg, const std::string & prefix = "", bool verbose
     << std::endl;
 }
 
+template<class MSG_T>
+void is_fixed_size()
+{
+  auto ts = rosidl_typesupport_cpp::get_message_type_support_handle<MSG_T>();
+  rmw_iceoryx_cpp::iceoryx_is_fixed_size(ts);
+}
+
+template<class MSG_T, size_t reps = 100, class UNIT = std::chrono::microseconds>
+void measure_type_introspection(const std::string & prefix = "", bool verbose = false)
+{
+  int64_t d_min = 999999999;
+  int64_t d_max = 0;
+  float d_std = 0.0;
+
+  for (auto i = 0u; i < reps; ++i) {
+    auto t1 = std::chrono::steady_clock::now();
+    is_fixed_size<MSG_T>();
+    auto t2 = std::chrono::steady_clock::now();
+    auto d_nano = std::chrono::duration_cast<UNIT>(t2 - t1).count();
+    if (verbose) {
+      std::cout << "rep: " << i << " = " << d_nano << std::endl;
+    }
+
+    d_min = std::min(d_nano, d_min);
+    d_max = std::max(d_nano, d_max);
+    d_std = (i * d_std + d_nano) / (i + 1);
+  }
+  std::cout << "[" << prefix << "]"
+    << " std: " << d_std
+    << " min: " << d_min
+    << " max: " << d_max
+    << std::endl;
+}
+
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
+
   auto bt_msgs = get_messages_basic_types();
   measure_serialize<test_msgs::msg::BasicTypes, 1>(bt_msgs[0].get(), "basic types");
   measure_serialize<test_msgs::msg::BasicTypes>(bt_msgs[0].get(), "basic types");
 
-  auto mn_msgs = get_messages_multi_nested();
-  measure_serialize<test_msgs::msg::BasicTypes>(mn_msgs[0].get(), "multi nested");
+  auto ubs_msgs = get_messages_unbounded_sequences();
+  measure_serialize<test_msgs::msg::UnboundedSequences>(ubs_msgs[0].get(), "unbounded sequences");
+
+  auto bs_msgs = get_messages_bounded_sequences();
+  measure_serialize<test_msgs::msg::BoundedSequences>(bs_msgs[0].get(), "bounded sequences");
+
+// SEQFAULT!
+//  auto mn_msgs = get_messages_multi_nested();
+//  measure_serialize<test_msgs::msg::MultiNested>(mn_msgs[0].get(), "multi nested", true);
+
+  measure_type_introspection<test_msgs::msg::MultiNested>();
   return 0;
 }
